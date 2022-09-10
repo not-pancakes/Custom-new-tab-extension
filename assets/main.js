39bd3707fe42f7add3eec5c263d9bc3b79ec4c86
Mikolaj Wojtowicz <70533837+not-pancakes@users.noreply.github.com>
2022-09-10 12:44:39 +0200

    Delete main.js

--- assets/main.js
+++
@@ -1,3 +0,0 @@
-$('.btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span><span></span></div>');
-
-$('.social-btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span></div>');
